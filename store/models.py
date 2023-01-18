18335a29e19d9610371928d156e4dfddf8c8bb94
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-01-18 20:18:57 +0400

    create model Posy

--- store/models.py
+++ store/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Posy(models.Model):
+    title = models.CharField(
+        verbose_name='название букета',
+        max_length=50,
+        blank=True
+    )
+    cause = models.CharField(
+        verbose_name='повод',
+        max_length=100,
+        blank=True
+    )
+    price = models.IntegerField(
+        'цена'
+    )
+    description = models.TextField(
+        verbose_name='описание букета',
+        blank=True,
+    )
+    picture = models.CharField(
+        verbose_name='изображение букета',
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+    composition = models.CharField(
+        verbose_name='состав',
+        max_length=200,
+    )
+
+    def __str__(self):
+        return self.title
