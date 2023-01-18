53952a08aef45dfb08e8cd81054fecdcd187f890
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-01-18 20:57:37 +0400

    create models Client, Florist, Courier

--- store/models.py
+++ store/models.py
@@ -32,3 +32,58 @@ class Posy(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Florist(models.Model):
+    full_name = models.CharField(
+        verbose_name="Имя",
+        max_length=50
+    )
+    client_key = models.ForeignKey(
+        "Client",
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+
+class Courier(models.Model):
+    full_name = models.CharField(
+        verbose_name="Имя",
+        max_length=50
+    )
+    client_key = models.ForeignKey(
+        "Client",
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+
+class Client(models.Model):
+    client_id = models.IntegerField(
+        verbose_name="Telegram ID"
+    )
+    full_name = models.CharField(
+        verbose_name="Имя",
+        max_length=50
+    )
+    phone_number = models.CharField(
+        verbose_name="Номер телефон",
+        max_length=12
+    )
+    address = models.TextField(
+        verbose_name="Адрес",
+        help_text='ул. Пушкина, д.103, кв.56'
+    )
+    delivery_datetime = models.DateTimeField(
+        name="Дата и время доставки"
+    )
+    florist_key = models.ForeignKey(
+        "Florist",
+        on_delete=models.CASCADE,
+        null=True
+    )
+    courier_key = models.ForeignKey(
+        "Courier",
+        on_delete=models.CASCADE,
+        null=True
+    )
