734c36843281d4a840a251ff891295c962136c21
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-01-20 00:38:17 +0400

    create admin models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Posy, Florist, Courier, Client
+
+
+@admin.register(Posy)
+class PosyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cause', 'price', 'description', 'picture', 'composition')
+    list_filter = ('title', 'cause', 'price')
+
+
+@admin.register(Florist)
+class FloristAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'client_key')
+
+
+@admin.register(Courier)
+class CourierAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'client_key')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('client_id', 'full_name', 'phone_number', 'address', 'florist_key', 'courier_key', 'delivery_datetime')
+    list_filter = ('client_id', 'phone_number')
