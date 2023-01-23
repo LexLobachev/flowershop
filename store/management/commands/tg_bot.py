e91a4125ed77fe9aaab4aa2a06ac4a4c267949fd
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-01-23 19:53:13 +0300

    Update tg_bot.py

--- store/management/commands/tg_bot.py
+++ store/management/commands/tg_bot.py
@@ -51,14 +51,21 @@ def send_image(bot, call, pic_number):
 def start(message):
     message_to_customer = f'Здравствуйте, <b>{message.from_user.first_name}</b>. К какому событию готовитесь? Выберите один из вариантов, либо укажите свой'
     markup = types.InlineKeyboardMarkup()
-    bday_button = types.InlineKeyboardButton('День рождения', callback_data= 'occ_birthday')
-    wedding_button = types.InlineKeyboardButton('Свадьба', callback_data= 'occ_wedding')
-    school_button = types.InlineKeyboardButton('Школа', callback_data= 'occ_school')
-    no_occation_button = types.InlineKeyboardButton('Нет повода', callback_data= 'occ_no')
-    other_occation_button = types.InlineKeyboardButton('Другой повод', callback_data= 'occ_other')
+    bday_button = types.InlineKeyboardButton('День рождения',
+                                             callback_data='occ_birthday')
+    wedding_button = types.InlineKeyboardButton('Свадьба',
+                                                callback_data='occ_wedding')
+    school_button = types.InlineKeyboardButton('Школа',
+                                               callback_data='occ_school')
+    no_occation_button = types.InlineKeyboardButton('Нет повода',
+                                                    callback_data='occ_no')
+    other_occation_button = types.InlineKeyboardButton('Другой повод',
+                                                       callback_data='occ_other')
     markup.add(bday_button, wedding_button, school_button, no_occation_button, other_occation_button)
-    bot.send_message(message.chat.id, message_to_customer, 
-                      parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id,
+                     message_to_customer, 
+                     parse_mode='html',
+                     reply_markup=markup)
     c, _ = Client.objects.update_or_create(
         client_id=message.chat.id,
         defaults={
@@ -78,31 +85,41 @@ def handle_occation(call):
         markup = types.InlineKeyboardMarkup(row_width=3)
         for price in prices:
             markup.add(types.InlineKeyboardButton(f'{price}', callback_data=f'price {price}'))
-        message = bot.send_message(call.message.chat.id, 'Вы готовитесь ко дню рождения, на какую сумму вы рассчитываете?',
-                         parse_mode='html', reply_markup=markup)
+        message = bot.send_message(call.message.chat.id,
+                                   'Вы готовитесь ко дню рождения, на какую сумму вы рассчитываете?',
+                                   parse_mode='html',
+                                   reply_markup=markup)
     elif call.data == 'occ_wedding':
         markup = types.InlineKeyboardMarkup(row_width=3)
         for price in prices:
             markup.add(types.InlineKeyboardButton(f'{price}', callback_data=f'price {price}'))
-        message = bot.send_message(call.message.chat.id, 'Вы готовитесь к свадьбе, на какую сумму вы рассчитываете?',
-                         parse_mode='html', reply_markup=markup)
+        message = bot.send_message(call.message.chat.id,
+                                   'Вы готовитесь к свадьбе, на какую сумму вы рассчитываете?',
+                                   parse_mode='html',
+                                   reply_markup=markup)
     elif call.data == 'occ_school':
         markup = types.InlineKeyboardMarkup(row_width=3)
         for price in prices:
             markup.add(types.InlineKeyboardButton(f'{price}', callback_data=f'price {price}'))
-        message = bot.send_message(call.message.chat.id, 'Вы готовитесь к школе, на какую сумму вы рассчитываете?',
-                         parse_mode='html', reply_markup=markup)
+        message = bot.send_message(call.message.chat.id,
+                                   'Вы готовитесь к школе, на какую сумму вы рассчитываете?',
+                                   parse_mode='html',
+                                   reply_markup=markup)
     elif call.data == 'occ_no':
         markup = types.InlineKeyboardMarkup(row_width=3)
         for price in prices:
             markup.add(types.InlineKeyboardButton(f'{price}', callback_data=f'price {price}'))
-        message = bot.send_message(call.message.chat.id, 'Отсутствие повода - тоже повод! На какую сумму Вы рассчитываете?',
-                         parse_mode='html', reply_markup=markup)
+        message = bot.send_message(call.message.chat.id,
+                                   'Отсутствие повода - тоже повод! На какую сумму Вы рассчитываете?',
+                                   parse_mode='html',
+                                   reply_markup=markup)
     elif call.data == 'occ_other':
         markup = types.InlineKeyboardMarkup(row_width=3)
         message_to_customer = 'Опишите кратко Ваш случай.'
-        message = bot.send_message(call.message.chat.id, message_to_customer,
-                         parse_mode='html', reply_markup=markup)
+        message = bot.send_message(call.message.chat.id,
+                                   message_to_customer,
+                                   parse_mode='html',
+                                   reply_markup=markup)
         bot.register_next_step_handler(message, handle_other_occation)
 
 
@@ -117,7 +134,10 @@ def handle_other_occation(message):
     markup = types.InlineKeyboardMarkup(row_width=3)
     for price in prices:
         markup.add(types.InlineKeyboardButton(f'{price}', callback_data=f'price {price}'))
-    bot.send_message(message.chat.id, message_to_customer, parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id,
+                     message_to_customer,
+                     parse_mode='html',
+                     reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('price'))
@@ -140,7 +160,10 @@ def handle_price(call):
     pic_number = posy.id
     real_price = posy.price
     send_image(bot, call, pic_number)
-    message = bot.send_message(call.message.chat.id, f'Ваш букет, стоимостью {real_price}', parse_mode='html', reply_markup=markup)
+    message = bot.send_message(call.message.chat.id,
+                               f'Ваш букет, стоимостью {real_price}',
+                               parse_mode='html',
+                               reply_markup=markup)
     bot.register_next_step_handler(message, handle_bouquet, pic_number)
 
 
@@ -154,17 +177,24 @@ def handle_bouquet(message, pic_number):
     )
     print(pic_number, 'its a chosen posy')
     if message.text == 'Заказать букет':
-        message = bot.send_message(message.chat.id, f'Ваш заказ будет передан курьеру. Напишите ваши данные. Имя:',
+        message = bot.send_message(message.chat.id,
+                                   f'Ваш заказ будет передан курьеру. Напишите ваши данные. Имя:',
                                    parse_mode='html')
         bot.register_next_step_handler(message, handle_user_name)
     elif message.text == 'Не подходит':
         message_to_customer = 'Хотите что-то более уникальное? Подберите другой букет из нашей коллекции или закажите консультацию флориста.'
         markup = types.InlineKeyboardMarkup(row_width=1)
-        consultation = types.InlineKeyboardButton(text='Консультация специалиста', callback_data='fin_consultation')
-        collection = types.InlineKeyboardButton(text='Посмотреть коллекцию', callback_data='fin_collection')
-        cancel = types.InlineKeyboardButton(text='Отменить', callback_data='fin_cancel')
+        consultation = types.InlineKeyboardButton(text='Консультация специалиста',
+                                                  callback_data='fin_consultation')
+        collection = types.InlineKeyboardButton(text='Посмотреть коллекцию',
+                                                callback_data='fin_collection')
+        cancel = types.InlineKeyboardButton(text='Отменить',
+                                            callback_data='fin_cancel')
         markup.add(consultation, collection, cancel)
-        bot.send_message(message.chat.id, message_to_customer, parse_mode='html', reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         message_to_customer,
+                         parse_mode='html',
+                         reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
@@ -176,7 +206,8 @@ def handle_user_name(message):
         }
     )
     print(message.text, 'its full name saving')
-    message = bot.send_message(message.chat.id, f'Адрес: ',
+    message = bot.send_message(message.chat.id,
+                               f'Адрес: ',
                                parse_mode='html')
     bot.register_next_step_handler(message, handle_user_adress)
 
@@ -190,13 +221,15 @@ def handle_user_adress(message):
         }
     )
     print(message.text, 'its address saving')
-    message = bot.send_message(message.chat.id, f'Желаемое время доставки:',
+    message = bot.send_message(message.chat.id,
+                               f'Желаемое время доставки: ',
                                parse_mode='html')
     bot.register_next_step_handler(message, handle_user_delivery_time)
 
 
 @bot.message_handler(content_types=['text'])
 def handle_user_delivery_time(message):
+    message_to_customer = 'Спасибо за Ваш заказ. Если хотите сделать другой заказ, напишите сообщение: "/start".',
     c, _ = Client.objects.update_or_create(
         client_id=message.chat.id,
         defaults={
@@ -204,33 +237,25 @@ def handle_user_delivery_time(message):
         }
     )
     print(message.text, 'its address saving')
-    message = bot.send_message(message.chat.id, f'Номер телефона:',
+    message = bot.send_message(message.chat.id,
+                               f'Номер телефона: ',
                                parse_mode='html')
-    bot.register_next_step_handler(message, handle_user_phone)
-
-
-@bot.message_handler(content_types=['text'])
-def handle_user_phone(message):
-    try:
-        if carrier._is_mobile(number_type(phonenumbers.parse(message.text))) and len(message.text)==12:
-            message = bot.send_message(message.chat.id, f'Спасибо за Ваш заказ. Если хотите сделать другой заказ, напишите сообщение: "/start" ',
-                                       parse_mode='html')
-            bot.register_next_step_handler(message, start)
-    except:
-        message = bot.send_message(message.chat.id, 'Вами был введен неверный номер, попробуйте ввести через +7')
-        bot.register_next_step_handler(message, handle_user_phone)
-        pass
+    bot.register_next_step_handler(message, handle_user_phone_number, message_to_customer)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('fin'))
 def handle_not_aproach(call):
     if call.data == 'fin_consultation':
+        message_to_customer = 'Спасибо за ваш отклик. Пока ожидаете звонка, предлагаем Вам ознакомиться со всей коллекцией.'
         message_to_customer = 'Укажите номер телефона, и наш флорист перезвонит Вам в течение 20 минут.'
-        message = bot.send_message(call.message.chat.id, message_to_customer, parse_mode='html')
-        bot.register_next_step_handler(message, handle_user_phone_number)
+        message = bot.send_message(call.message.chat.id,
+                                   message_to_customer,
+                                   parse_mode='html')
+        bot.register_next_step_handler(message, handle_user_phone_number, message_to_customer)
     elif call.data == 'fin_collection':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
-        bot.send_message(call.message.chat.id, 'Готовые букеты для вас:')
+        bot.send_message(call.message.chat.id,
+                         'Готовые букеты для вас:')
         for key, value in ready_made_posys().items():
             script_path = pathlib.Path.cwd()
             file_path = script_path.joinpath(f'flowershop/media/{value["picture"]}')
@@ -241,10 +266,13 @@ def handle_not_aproach(call):
                     caption=f"{key}. {value['title']} \nЦена: {str(value['price'])} руб."
                 )
                 markup.add(types.KeyboardButton(text=f'{key}'))
-        message = bot.send_message(call.message.chat.id, 'Какой из предложенных букетов Вас интересует?', reply_markup=markup)
+        message = bot.send_message(call.message.chat.id,
+                                   'Какой из предложенных букетов Вас интересует?',
+                                   reply_markup=markup)
         bot.register_next_step_handler(message, handle_user_choice)
     elif call.data == 'fin_cancel':
-        message = bot.send_message(call.message.chat.id, 'Если хотите выбрать другой букет, напишите сообщение "/start".')
+        message = bot.send_message(call.message.chat.id,
+                                   'Если хотите выбрать другой букет, напишите сообщение "/start".')
         bot.register_next_step_handler(message, start)
 
 
@@ -255,7 +283,8 @@ def handle_user_choice(message):
         script_path = pathlib.Path.cwd()
         file_path = script_path.joinpath(f'flowershop/media/posy_{message.text}.jpg')
         with open(file_path, 'rb') as posting_file:
-            bot.send_photo(chat_id=message.chat.id, photo=posting_file)
+            bot.send_photo(chat_id=message.chat.id,
+                           photo=posting_file)
         c, _ = Client.objects.update_or_create(
             client_id=message.chat.id,
             defaults={
@@ -267,13 +296,15 @@ def handle_user_choice(message):
         order = types.KeyboardButton(text='Заказать букет')
         other = types.KeyboardButton(text='Не подходит')
         markup.add(order, other)
-        message = bot.send_message(message.chat.id, 'Ваш букет',
-                         parse_mode='html', reply_markup=markup)
+        message = bot.send_message(message.chat.id,
+                                   'Ваш букет',
+                                   parse_mode='html',
+                                   reply_markup=markup)
         bot.register_next_step_handler(message, handle_bouquet)
 
 
 @bot.message_handler(content_types=['text'])
-def handle_user_phone_number(message):
+def handle_user_phone_number(message, message_to_customer):
     my_string_number = message.text
     try:
         my_number = phonenumbers.parse(my_string_number)
@@ -290,14 +321,24 @@ def handle_user_phone_number(message):
             }
         )
         print(my_string_number, 'its phone_number saving')
-        markup = types.InlineKeyboardMarkup()
-        collection_button = types.InlineKeyboardButton(text='Коллеция', callback_data='fin_collection')
-        cancel_button = types.InlineKeyboardButton(text='Отменить', callback_data='fin_cancel')
-        markup.add(collection_button, cancel_button)
-        message = bot.send_message(message.chat.id,
-                                   f'Спасибо за ваш отклик. Для возврата в начальное меню нажмите: "/start" ',
-                                   parse_mode='html', reply_markup=markup)
-        bot.register_next_step_handler(message, start)
+        if message_to_customer == 'Спасибо за ваш отклик. Пока ожидаете звонка, предлагаем Вам ознакомиться со всей коллекцией.':
+            markup = types.InlineKeyboardMarkup()
+            collection_button = types.InlineKeyboardButton(text='Коллеция',
+                                                           callback_data='fin_collection')
+            cancel_button = types.InlineKeyboardButton(text='Отменить',
+                                                       callback_data='fin_cancel')
+            markup.add(collection_button, cancel_button)
+            message = bot.send_message(message.chat.id,
+                                       message_to_customer,
+                                       parse_mode='html',
+                                       reply_markup=markup)
+        elif message_to_customer == 'Спасибо за Ваш заказ. Если хотите сделать другой заказ, напишите сообщение: "/start".'
+            markup.add(collection_button, cancel_button)
+            message = bot.send_message(message.chat.id,
+                                       message_to_customer,
+                                       parse_mode='html',
+                                       reply_markup=markup)
+            bot.register_next_step_handler(message, start)
     else:
         message = bot.send_message(message.chat.id,
                                    f'Введен некорректный номер телефона. Попробуйте еще раз',
